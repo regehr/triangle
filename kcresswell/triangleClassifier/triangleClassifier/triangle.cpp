57d3a65934845eb7ee164e938535d8e427f64afd
kcresswell <makayla.cresswell@gmail.com>
2018-01-09 18:27:32 -0700

    just need to test it

--- kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
+++ kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
@@ -9,47 +9,51 @@
 #include <iostream>
 #include <cmath>
 
-std::string getTriangleType(int x1, int y1, int x2, int y2, int x3, int y3){
+std::string getTriangleType(int aX, int aY, int bX, int bY, int cX, int cY){
     //calculate each side length
     //d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
-    float length1, length2, length3;
-    length1 = sqrt((x2 - x1)^2 + (y2 - y1)^2);
-    length2 = sqrt((x3 - x1)^2 + (y3 - y1)^2);
-    length3 = sqrt((x3 - x2)^2 + (y3 - y2)^2);
+    float lengthA, lengthB, lengthC;
+    lengthA = sqrt((cX - bX)^2 + (cY - bY)^2);
+    lengthB = sqrt((cX - aX)^2 + (cY - aY)^2);
+    lengthC = sqrt((bX - aX)^2 + (bY - aY)^2);
     
     //calculate each angle
-    float angle1, angle2, angle3;
-    angle1 = acos((length3 + length2 - length1)/(2*length3*length2)); //A
-    angle3 = acos((length2 + length1 - length3)/(2*length2*length1)); //B
-    angle2 = 180 - angle1 - angle3;
+    float angleAlpha, angleBravo, angleCharlie;
+    angleAlpha = acos((pow(lengthB, 2.0) + pow(lengthC, 2.0) - pow(lengthA, 2.0)) / 2 * lengthB * lengthC);
+    angleBravo = acos((pow(lengthC, 2.0) + pow(lengthA, 2.0) - pow(lengthB, 2.0)) / 2 * lengthC * lengthA);
+    angleCharlie = acos((pow(lengthA, 2.0) + pow(lengthB, 2.0) - pow(lengthC, 2.0)) / 2 * lengthA * lengthB);
     
     //right triangle: one 90 degree angle
-    if(angle1 == 90.0 || angle2 == 90.0 || angle3 == 90.0){
-        std::cout << "right/n";
+    if(angleAlpha == 90.0 || angleBravo == 90.0 || angleCharlie == 90.0){
+        std::cout << "right\n";
     }
     
     //isosceles: two sides same length
-    else if(length1 == length2 || length1 == length3 || length2 == length3){
-        std::cout << "isosceles/n";
+    else if(lengthA == lengthB || lengthA == lengthC || lengthB == lengthC){
+        std::cout << "isosceles\n";
     }
     
     //equilateral: all sides same length
-    else if(length1 == length2 && length1 == length3){
-        std::cout << "equilateral/n";
+    else if(lengthA == lengthB && lengthA == lengthC){
+        std::cout << "equilateral\n";
     }
     
     //obtuse: one angle >90
-    else if(angle1 > 90 || angle2 > 90 || angle3 > 90){
-        std::cout << "obtuse/n";
+    else if(angleAlpha > 90 || angleBravo > 90 || angleCharlie > 90){
+        std::cout << "obtuse\n";
     }
     
     //acute: all angles <90
-    else if(angle1 < 90 || angle2 < 90 || angle3 < 90){
-        std::cout << "acute/n";
+    else if(angleAlpha < 90 || angleBravo < 90 || angleCharlie < 90){
+        std::cout << "acute\n";
     }
     
     //scalene: none of the above
-    return "scalene";
+    else{
+        std::cout << "scalene\n";
+    }
+    
+    return "";
     
 }
 
@@ -57,7 +61,7 @@ int main(int argc, const char * argv[]) {
     int x1, y1, x2, y2, x3, y3 = 0;
     std::cout << "Enter shape coordinates in the following format x1 y1 x2 y2 x3 y3: ";
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    std::cout << "You entered: " << x1 << " " << y1 << " " << x2  << " " << y2  << " " << x3  << " " << y3;
+    std::cout << "You entered: " << x1 << " " << y1 << " " << x2  << " " << y2  << " " << x3  << " " << y3 << "\n";
     std::cout << "Triangle Type: " << getTriangleType(x1, y1, x2, y2, x3, y3);
     
     return 0;
