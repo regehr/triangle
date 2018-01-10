1fdf25a33dc138dc5fde88f9dac10aad1789eaa3
Eric <truthdefender02@yahoo.com>
2018-01-09 20:08:26 -0700

    updated to find length of triangle.

--- chiefLachoneus/SpecifyTriangle/SpecifyTriangle/triangle.cpp
+++ chiefLachoneus/SpecifyTriangle/SpecifyTriangle/triangle.cpp
@@ -7,19 +7,22 @@
 //
 
 #include <iostream>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
     int x1, y1, x2, y2, x3, y3;
     
-    std::cout << "Please provide 3 points for a triangle in this format (x1 y1 x2 y2 x3 y3): \n";
+    std::cout << "Please provide 3 points for a triangle in this format (x1 y1 x2 y2 x3 y3):" << std::endl;
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     
     std::cout << "Your responses: " << x1 << " " << y1  << " " << x2 << " " << y2 << " " << x3 << " " << y3 << std::endl;
     std::cout << "Your responses: " << x1 << y1 << x2 << y2 << x3 << y3  << std::endl;
     
+    
+    
     return 0;
 }
 
-void getTriangle(){
-//    std::cin <<
+float findLength(int coorX1, int coorY1, int coorX2, int coorY2){
+    return sqrtf(pow((coorX2 - coorX1),2) + pow((coorY2 - coorY1),2));
 }
