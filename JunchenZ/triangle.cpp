df27d9b829bdbd3a6b7c09dc7ab8dbcf0298197a
JunchenZ <jack_zhangjunchen@163.com>
2018-01-11 11:01:18 -0700

    unsolved questions

--- JunchenZ/triangle.cpp
+++ JunchenZ/triangle.cpp
@@ -3,51 +3,56 @@
 
 using namespace std;
 
+//while true loop?
+//check input?
+//delete unused cases?
+
 int main(int argc, const char * argv[]) {
     
-    int coord[3][2];
+    int x1, y1, x2, y2, x3, y3;
     
-    cout << "please enter 6 integers from 0 to 100 as 3 pairs of corrdinates to form a shape" << endl;
+    cout << "please enter 6 integers from 0 to 100 as 3 pairs of corrdinates to form a triangle" << endl;
     
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            cin >> coord[i][j];
-        }
-    }
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     
-    int side1 = pow((coord[0][0] - coord[1][0]), 2) + pow((coord[0][1] - coord[1][1]), 2);
-
-    int side2 = pow((coord[2][0] - coord[1][0]), 2) + pow((coord[2][1] - coord[1][1]), 2);
-
-    int side3 = pow((coord[2][0] - coord[0][0]), 2) + pow((coord[2][1] - coord[0][1]), 2);
-
-    if((sqrt(side1) + sqrt(side2)) <= sqrt(side3) || (sqrt(side3) + sqrt(side2) <= sqrt(side1)) || (sqrt(side1) + sqrt(side3)) <= sqrt(side2)){
-        cout << "it is not a triangle" << endl;
+    //calculate square of length of each side
+    int s1 = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    int s2 = pow((x3 - x1), 2) + pow((y3 - y1), 2);
+    int s3 = pow((x3 - x2), 2) + pow((y3 - y2), 2);
+    
+    if((sqrt(s1) + sqrt(s2) <= sqrt(s3)) || (sqrt(s3) + sqrt(s2) <= sqrt(s1)) || (sqrt(s1) + sqrt(s3) <= sqrt(s2))){
+        cout << "degenerate" << endl;
         return 0;
     }
     
-    if((side1 + side2 == side3) || (side1 + side3 == side2) || (side3 + side2 == side1)){
-        cout << "it is a right triangle" << endl;
+    if((s1 + s2 == s3) || (s1 + s3 == s2) || (s3 + s2 == s1)){
+        cout << "right triangle" << endl;
+        return 0;
     }
     
-    if((side1 + side2 < side3) || (side1 + side3 < side2) || (side3 + side2 < side1)){
-        cout << "it is an obtuse triangle" << endl;
+    if(s1 == s2 || s3 == s2 || s3 == s1){
+        cout << "isosceles" << endl;
+        return 0;
     }
     
-    if((side1 + side2 > side3) && (side1 + side3 > side2) && (side3 + side2 > side1)){
-        cout << "it is an acute triangle" << endl;
+    if(s3 == s2 && s3 == s1){
+        cout << "equilateral" << endl;
+        return 0;
     }
     
-    if(side1 == side2 || side3 == side2 || side3 == side1){
-        cout << "it is an isosceles triangle" << endl;
+    if((s1 + s2 < s3) || (s1 + s3 < s2) || (s3 + s2 < s1)){
+        cout << "obtuse" << endl;
+        return 0;
     }
     
-    if(side3 == side2 && side3 == side1){
-        cout << "it is an equilateral triangle" << endl;
+    if((s1 + s2 > s3) && (s1 + s3 > s2) && (s3 + s2 > s1)){
+        cout << "acute" << endl;
+        return 0;
     }
     
-    if(side3 != side2 && side3 != side1 && side2 != side1){
-        cout << "it is a scalene triangle" << endl;
+    if(s3 != s2 && s3 != s1 && s2 != s1){
+        cout << "scalene" << endl;
+        return 0;
     }
     
 }
