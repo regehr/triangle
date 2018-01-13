806d53e67d00758769f941cfdc7baee59d2b59af
Matt Josse <matt.m.josse@gmail.com>
2018-01-13 14:22:21 -0700

    clang format

--- mjosc/triangle.cpp
+++ mjosc/triangle.cpp
@@ -31,16 +31,16 @@ struct Triangle {
 // Calculates side length and angles (in degrees). Implements the distance
 // formula and the law of cosines.
 Triangle::Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
-: x1(x1), y1(y1), x2(x2), y2(y2), x3(x3), y3(y3) {
+    : x1(x1), y1(y1), x2(x2), y2(y2), x3(x3), y3(y3) {
   len1 = sqrt((pow(x2 - x1, 2)) + (pow(y2 - y1, 2)));
   len2 = sqrt((pow(x3 - x2, 2)) + (pow(y3 - y2, 2)));
   len3 = sqrt((pow(x1 - x3, 2)) + (pow(y1 - y3, 2)));
   ang1 =
-  acos(((pow(len2, 2) + pow(len3, 2)) - pow(len1, 2)) / (2 * len2 * len3)) *
-  180 / PI;
+      acos(((pow(len2, 2) + pow(len3, 2)) - pow(len1, 2)) / (2 * len2 * len3)) *
+      180 / PI;
   ang2 =
-  acos(((pow(len3, 2) + pow(len1, 2)) - pow(len2, 2)) / (2 * len3 * len1)) *
-  180 / PI;
+      acos(((pow(len3, 2) + pow(len1, 2)) - pow(len2, 2)) / (2 * len3 * len1)) *
+      180 / PI;
   ang3 = 180 - (ang1 + ang2);
 }
 
@@ -57,7 +57,7 @@ bool Triangle::is_valid() {
 bool Triangle::is_right() {
   const double e = 0.0001;
   return is_valid() &&
-  (abs(ang1 - 90.0) < e || abs(ang2 - 90.0) < e || abs(ang3 - 90.0) < e);
+         (abs(ang1 - 90.0) < e || abs(ang2 - 90.0) < e || abs(ang3 - 90.0) < e);
 }
 
 // Returns true if a valid triangle and at least two of the three side lengths
