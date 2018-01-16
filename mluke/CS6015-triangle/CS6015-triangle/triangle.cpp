2ca7de17527dfc986e1b676f9709659a5ac6e807
Madeline Luke <madeline.luke@utah.edu>
2018-01-16 09:43:37 -0700

    tues triangle

--- mluke/CS6015-triangle/CS6015-triangle/triangle.cpp
+++ mluke/CS6015-triangle/CS6015-triangle/triangle.cpp
@@ -6,67 +6,55 @@
 //  Copyright © 2018 Madeline Luke. All rights reserved.
 //
 
+#include <cmath>
 #include <iostream>
 #include <math.h>
-#include <cmath>
 
 float findDistance(int x, int y, int x1, int y1) {
-    int value = pow((x1 - x),2) + pow((y1 - y),2) ;
-    return sqrt(value) ;
+  int value = pow((x1 - x), 2) + pow((y1 - y), 2);
+  return sqrt(value);
 }
 
-float findAngle(int x, int y, int z) {
-    float val = (pow(x, 2) + pow(y, 2) - pow(z, 2)) / (2*x*y) ;
-    return acos(val) * (180.0/3.14);
+double findAngle(double x, double y, double z) {
+  float val = ((pow(x, 2) + pow(y, 2) - pow(z, 2))) / (2 * x * y);
+  return acos(val) * (180.0 / 3.14);
 }
 
 void triangleClassifier(int x1, int y1, int x2, int y2, int x3, int y3) {
-    float side1, side2, side3 ;
-    side1 = findDistance(x1, y1, x2, y2) ;
-    side2 = findDistance(x1, y1, x3, y3) ;
-    side3 = findDistance(x2, y2, x3, y3) ;
-    
-    float angleA, angleB, angleC ;
-    angleC = findAngle(side1, side2, side3) ;
-    angleA = findAngle(side2, side3, side1) ;
-    angleB = findAngle(side3, side1, side2) ;
-    
-    if (angleA == 90.0 || angleB == 90.0 || angleC == 90.0) {
-        std:: cout << "This is a right triangle. \n" ;
-    } else if (side1 == side2 || side1 == side3 || side2 == side3) {
-        std::cout << "This is an isosceles triangle. \n" ;
-    } else if (side1 == side2 || side2 == side3) {
-        std::cout << "This is an equilateral triangle. \n" ;
-    } else if (angleA > 90 || angleB > 90 || angleC > 90) {
-        std::cout << "This is an obtuse triangle. \n" ;
-    } else if (angleA < 90 && angleB < 90 && angleC < 90) {
-        std::cout << "This is an acute angle. \n" ;
+  float side1, side2, side3;
+  side1 = findDistance(x1, y1, x2, y2);
+  side2 = findDistance(x1, y1, x3, y3);
+  side3 = findDistance(x2, y2, x3, y3);
+
+  double angleA, angleB, angleC;
+  angleC = round(findAngle(side1, side2, side3));
+  angleA = round(findAngle(side2, side3, side1));
+  angleB = round(findAngle(side3, side1, side2));
+
+  if (angleA == 90.0 || angleB == 90.0 || angleC == 90.0) {
+    std::cout << "right \n";
+  } else if (side1 == side2 || side1 == side3 || side2 == side3) {
+    std::cout << "isosceles \n";
+  } else if (angleA > 90 || angleB > 90 || angleC > 90) {
+    std::cout << "obtuse \n";
+  } else if (angleA < 90 && angleB < 90 && angleC < 90) {
+    std::cout << "acute \n";
+  } else if ((x1 == x2 && y1 == y2) || (x1 == x3 && y1 == y3) ||
+             (x2 == x3 && y2 == y3)) {
+    std::cout << "degenerate \n";
+  }
+}
+
+int main(int argc, const char *argv[]) {
+
+  int x1, y1, x2, y2, x3, y3;
+
+  while (true) {
+    if (!(std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3)) {
+      break;
     }
-    
-}
+    triangleClassifier(x1, y1, x2, y2, x3, y3);
+  }
 
-int main(int argc, const char * argv[]) {
-    
-    int triangleAmount ;
-    int x1, y1, x2, y2, x3, y3 ;
-    std::cout << "How many triangles? \n" ;
-    std::cin >> triangleAmount ;
-
-    for (int i = 0 ; i < triangleAmount ; i++) {
-        std::cout << "Enter 6 points for a triangle. \n" ;
-        std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 ;
-        triangleClassifier(x1, y1, x2, y2, x3, y3) ;
-}
-    
-    // Right triangle.
-    triangleClassifier(-1, 1, 4, 4, 6, 2) ;
-    // Isosceleses.
-    triangleClassifier(8, 2, 11, 13, 2, 6) ;
-    triangleClassifier(-3, -3, 0, 5, 3, -3) ;
-    // Obtuse
-    triangleClassifier(0, 0, -1, 4, 4, 0) ;
-    //Acute
-    triangleClassifier(9, 0, 0, 0, 4, 6) ;
-    
   return 0;
 }
