cc883a3ea3861ba4f2e43daeedec3540c2f68757
Chris Roper <croper@alumni.nd.edu>
2018-01-16 14:04:12 -0700

    triangle fixes

--- roper4597/triangle.cpp
+++ roper4597/triangle.cpp
@@ -12,9 +12,12 @@
 #include <cassert>
 #include <cmath>
 #include <vector>
-double pi = 3.1415926535897;
+const double pi = 3.1415926535897;
 
 double slope(double x1, double x2, double y1, double y2) {
+    if(x1 == x2 || y1 == y2) {
+        return -99;
+    }
     return (x1-x2)/(y1-y2);
 }
 
@@ -28,39 +31,48 @@ double radiansToDegrees(double radians) {
 std::string getTriangleType(int x1, int y1, int x2, int y2, int x3, int y3) {
     //equal points check
     if((x1 == x2 && y1 == y2) || (x2 == x3 && y2 == y3) || (x1 == x3 && y1 == y3)) {
-        return "Degenerate\n";
+        return "degenerate";
     }
     //all points on same x,y line check
     if((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)) {
-        return "Degenerate\n";
+        return "degenerate";
     }
     //slope check (assumes points are not equal - other cases are caught above)
     if(x1 != x2 && x2 != x3 && y1 != y2 && y2 != y3) {
         if(slope(x1, x2, y1, y2) == slope(x2, x3, y2, y3)) {
-        return "Degenerate\n";
+        return "degenerate";
         }
     }
+    
+//    std::cout << slope(x1, x2, y1, y2) << "\n";
+//    std::cout << slope(x2, x3, y2, y3) << "\n";
+//    std::cout << slope(x1, x2, y2, y3) << "\n";
     double lengthC = sqrt(pow((x1-x2),2)+pow((y1-y2),2));
     double lengthA = sqrt(pow((x2-x3),2)+pow((y2-y3),2));
     double lengthB = sqrt(pow((x3-x1),2)+pow((y3-y1),2));
     assert(lengthA > 0 && lengthB > 0 && lengthC > 0);
     
-    double angleC = round(10*(radiansToDegrees(acos((pow(lengthA,2)+pow(lengthB,2)-pow(lengthC,2))/(2*lengthA*lengthB))))/10);
-    double angleB = round(10*(radiansToDegrees(asin(lengthB*(sin(degreesToRadians(angleC))/lengthC))))/10);
+    double angleC = (radiansToDegrees(acos((pow(lengthA,2)+pow(lengthB,2)-pow(lengthC,2))/(2*lengthA*lengthB))));
+    double angleB = (radiansToDegrees(acos((pow(lengthA,2)+pow(lengthC,2)-pow(lengthB,2))/(2*lengthA*lengthC))));
+    //double angleB = (radiansToDegrees(asin(lengthB*(sin(degreesToRadians(angleC))/lengthC))));
     double angleA = 180 - angleC - angleB;
     assert(angleA > 0 && angleB > 0 && angleC > 0);
-//    std::cout << lengthA << " " << lengthB << " " << lengthC << "\n";
-//    std::cout << angleA << " " << angleB << " " << angleC << "\n";
+    assert((angleA + angleB + angleC) < 180.01);
+    angleC = round(angleC);
+    angleB = round(angleB);
+    angleA = round(angleA);
+//        std::cout << lengthA << " " << lengthB << " " << lengthC << "\n";
+//        std::cout << angleA << " " << angleB << " " << angleC << "\n";
     if (angleA == 90 || angleB == 90 || angleC == 90) {
-        return "Right\n";
+        return "right";
     }
     else if ((angleA == angleB) || (angleB == angleC) || (angleC == angleA) || (lengthA == lengthB) || (lengthB==lengthC) || (lengthC==lengthA)) {
-        return "Isosceles\n";
+        return "isosceles";
     }
     else if (angleA < 90 && angleB < 90 && angleC < 90) {
-        return "Acute\n";
+        return "acute";
     }
-    else return "Obtuse\n";
+    else return "obtuse";
 }
 
 int main(int argc, const char * argv[]) {
