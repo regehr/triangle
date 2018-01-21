9bd0c4e9cb18950b1d4d7d567fd5f4633b5d8ae9
kcresswell <makayla.cresswell@gmail.com>
2018-01-21 11:41:22 -0700

    added print statement that was missing so the empty quotes error should be resolved

--- kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
+++ kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
@@ -84,86 +84,9 @@ int main(int argc, const char * argv[]) {
         }
         
         // Get the type of the triangle
-        determineTriangleType(x1, y1, x2, y2, x3, y3);
-        
+        std::cout << determineTriangleType(x1, y1, x2, y2, x3, y3);
     }
     
     return 0;
 }
-
-//std::string getTriangleType(int aX, int aY, int bX, int bY, int cX, int cY){
-//    //calculate each side length
-////    float lengthA, lengthB, lengthC = 0;
-////    lengthA = round(sqrt(pow(cX - bX, 2) + pow(cY - bY, 2)));
-////    lengthB = round(sqrt(pow(cX - aX, 2) + pow(cY - aY, 2)));
-////    lengthC = round(sqrt(pow(bX - aX, 2) + pow(bY - aY, 2)));
-////
-////    //calculate each angle
-////    float angleAlpha, angleBravo, angleCharlie = 0;
-////    angleAlpha = round(acos((pow(lengthB, 2.0) + pow(lengthC, 2.0) - pow(lengthA, 2.0)) / 2 * lengthB * lengthC));
-////    angleBravo = round(acos((pow(lengthC, 2.0) + pow(lengthA, 2.0) - pow(lengthB, 2.0)) / 2 * lengthC * lengthA));
-////    angleCharlie = round(acos((pow(lengthA, 2.0) + pow(lengthB, 2.0) - pow(lengthC, 2.0)) / 2 * lengthA * lengthB));
-//
-//    //from myriah
-//    double lengthA = findLineLength(aX, aY, bX, bY);
-//    double lengthB = findLineLength(aX, aY, cX, cY);
-//    double lengthC = findLineLength(bX, bY, cX, cY);
-//
-//    double angleAlpha = findAngle(lengthA, lengthB, lengthC);
-//    double angleBravo = findAngle(lengthB, lengthA, lengthC);
-//    double angleCharlie = 180 - angleAlpha - angleBravo;
-//
-//    //right triangle: one 90 degree angle
-//    //if(abs(lengthA * lengthA + lengthB * lengthB - lengthC * lengthC) <=
-//     //  0.0001){
-//    if(angleAlpha == 90.0 || angleBravo == 90.0 || angleCharlie == 90.0){
-//        std::cout << "right\n";
-//    }
-////    if (angleAlpha == 90.0 || angleBravo == 90.0 || angleCharlie == 90.0) {
-////        std::cout << "right\n";
-////    }
-//
-//    //isosceles: two sides same length
-//    else if(lengthA == lengthB || lengthA == lengthC || lengthB == lengthC){
-//        std::cout << "isosceles\n";
-//    }
-//
-//    //obtuse: one angle >90
-//    else if(angleAlpha > 90.0 || angleBravo > 90.0 || angleCharlie > 90.0){
-//        std::cout << "obtuse\n";
-//    }
-//
-//    //equilateral: all sides same length
-//    else if(lengthA == lengthB && lengthA == lengthC){
-//        std::cout << "equilateral\n";
-//    }
-//
-//    //acute: all angles <90
-//    else if(angleAlpha < 90 && angleBravo < 90 && angleCharlie < 90){
-//        std::cout << "acute\n";
-//    }
-//
-//    //sum of two short sides must be > the hypotenuse
-//    else if(lengthA + lengthB <= lengthC){
-//        std::cout << "degenerate\n";
-//    }
-//
-//    //scalene: none of the above
-//    else{
-//        std::cout << "scalene\n";
-//    }
-//
-//    return "";
-//
-//}
-//
-//int main(int argc, const char * argv[]) {
-//    int x1, y1, x2, y2, x3, y3 = 0;
-//    std::cout << "Enter shape coordinates in the following format x1 y1 x2 y2 x3 y3: ";
-//    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-//    std::cout << "You entered: " << x1 << " " << y1 << " " << x2  << " " << y2  << " " << x3  << " " << y3 << "\n";
-//    std::cout << "Triangle Type: " << getTriangleType(x1, y1, x2, y2, x3, y3);
-//
-//    return 0;
-//}
 
