707ad1b5ee4e3810f7d5ea4778eaf1b3385b765c
kcresswell <makayla.cresswell@gmail.com>
2018-01-21 11:56:22 -0700

    added different degenerate check

--- kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
+++ kcresswell/triangleClassifier/triangleClassifier/triangle.cpp
@@ -11,13 +11,13 @@
 #define PI 3.14159265
 
 //distance formula
-double calcDistance(int x1, int y1, int x2, int y2){
+double calcDistance(int x1, int y1, int x2, int y2) {
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) );
 }
 
 
 //angle calc and radians conversion from doug
-double findAngle(double a, double b, double c){
+double findAngle(double a, double b, double c) {
     double math = (pow(a, 2) + pow(b, 2) - pow(c, 2)) / (2 * a * b);
     double comp = std::abs(acos( math ) * 180.0 / PI);
 
@@ -25,11 +25,11 @@ double findAngle(double a, double b, double c){
 }
 
 //from doug, margin or error needed to get right triangle to work in my code.
-bool doublesEqual(double a, double b){
+bool doublesEqual(double a, double b) {
     return std::abs(a-b) < 0.000001;
 }
 
-std::string determineTriangleType(int x1, int y1, int x2, int y2, int x3, int y3){
+std::string determineTriangleType(int x1, int y1, int x2, int y2, int x3, int y3) {
     
     // Determine the three side lengths
     double sideA = calcDistance(x1, y1, x2, y2);
@@ -41,28 +41,28 @@ std::string determineTriangleType(int x1, int y1, int x2, int y2, int x3, int y3
     double angleB = findAngle(sideC, sideA, sideB);
     double angleC = findAngle(sideA, sideB, sideC);
 
-    //if any sides are 0 then it is degenerate
-    if(sideA == 0.0 || sideB == 0.0 || sideC == 0.0 || (angleA + angleB + angleC < 180)) {
+    //degenerate check
+    if(sideA >= sideB + sideC || sideB >= sideA + sideC || sideC >= sideA + sideB || (angleA + angleB + angleC < 180)) {
         return "degenerate\n";
     }
     
     // If one of the angles is 90 degrees, right triangle
-    if (doublesEqual(angleA, 90.0) || doublesEqual(angleB, 90.0) || doublesEqual(angleC, 90.0)){
+    if (doublesEqual(angleA, 90.0) || doublesEqual(angleB, 90.0) || doublesEqual(angleC, 90.0)) {
         return "right\n";
     }
     
     // If two or more sides are the same, isosceles triangle
-    else if (doublesEqual(sideA, sideB) || doublesEqual(sideB, sideC) || doublesEqual(sideC, sideA)){
+    else if (doublesEqual(sideA, sideB) || doublesEqual(sideB, sideC) || doublesEqual(sideC, sideA)) {
         return "isosceles\n";
     }
     
     // If one of the angles is > 90 degrees, obtuse triangle
-    else if (angleA > 90 || angleB > 90 || angleC > 90){
+    else if (angleA > 90 || angleB > 90 || angleC > 90) {
         return "obtuse\n";
     }
     
     // If all angles are < 90 degrees
-    else if (angleA < 90 && angleB < 90 && angleC < 90){
+    else if (angleA < 90 && angleB < 90 && angleC < 90) {
         return "acute\n";
     }
     
