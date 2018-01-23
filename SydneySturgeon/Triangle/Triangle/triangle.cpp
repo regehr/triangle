b86da63373329b249832fa82f4a47ed2cbd5519a
SydneySturgeon <sydneys@cs.utah.edu>
2018-01-23 09:46:10 -0700

    tri

--- SydneySturgeon/Triangle/Triangle/triangle.cpp
+++ SydneySturgeon/Triangle/Triangle/triangle.cpp
@@ -73,7 +73,9 @@ void triangleClassifier(int x1, int y1, int x2, int y2, int x3, int y3) {
   // if any of the angles are <= 0 it is degenerate
   if ((angleA <= 0.0f) || (angleB <= 0.0f) || (angleC <= 0.0f)) {
     cout << "degenerate\n";
-  } else if (angleA == 90.0f || angleB == 90.0f || angleC == 90.0f) {
+  } else if (angleA == 90.0f || angleB == 90.0f || angleC == 90.0f
+             || angleA == 89.9f || angleB == 89.9f || angleC == 89.9f
+             || angleA == 90.1f || angleB == 90.1f || angleC == 90.1f){
     cout << "right\n";
   } else if ((lineLength12 == lineLength13 && lineLength12 != lineLength23) ||
              (lineLength13 == lineLength23 && lineLength13 != lineLength12) ||
